6fcc61b2debe32cb3dbab503e022faa2de5df64f
hamzaisrar16 <153775137+hamzaisrar16@users.noreply.github.com>
2024-05-01 00:15:40 +0500

    Toss App

---
+++ Toss App 2.0/main.js
@@ -0,0 +1,36 @@
+let h1 = document.querySelector('.head1');
+let h2 = document.querySelector('h2')
+let btn = document.querySelector('button')
+let img = document.querySelector('img')
+
+function reset(){
+    img.src="./Assets/Images/flip-coin.png"
+    h1.innerHTML = 'DO TOSS NOW!'
+    h2.innerHTML = ''
+}
+
+function tossNow(index){
+    console.log(index);
+    let randomNumber = Math.ceil(Math.random()*2)
+    console.log(randomNumber);
+    if(index === 'HEADS' && randomNumber === 1 || index === 'TAILS' && randomNumber === 2){
+        console.log('YOU WON THE TOSS!');
+        h2.innerHTML = 'CONGRATULAIONS! YOU WON THE TOSS!'
+        h1.innerHTML = index
+        img.src="./assets/coin-2.jpeg"
+    }else {
+        console.log('YOU LOSS THE TOSS!');
+        h1.innerHTML = index
+        h2.innerHTML = 'YOU LOSS THE TOSS!!'
+        img.src="./assets/coin.jpeg"
+    }
+}
+
+    
+
+
+
+
+
+
+
